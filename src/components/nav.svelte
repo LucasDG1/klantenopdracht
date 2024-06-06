7cb07652f24d904f9c1daca74b4bb05b0f280096
TheRealGeno <genokooijman@gmail.com>
2024-06-06 12:07:21 +0200

    navbar fix

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,12 +1,12 @@
-<nav  class="w-full fixed top-0 z-50">
-	<div class="container-fluid">
+<nav class="w-screen fixed top-0 z-50">
+	<div class=" w-full">
 		<p class="bg-black text-white">
 			Track work across the enterprise through an open, collaborative platform. Link issues across
 			Jira and ingest data from other software development tools, so your IT support and operations
 			teams have richer contextual information to rapidly respond to requests, incidents, and
 			changes.
 		</p>
-		<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+		<hr class="bg-[#525F4F] border-1 dark:bg-black" />
 		<p class="bg-black text-white">
 			Deliver great service experiences fast - without the complexity of traditional ITSM
 			solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <div class="grid grid-cols-6 gap-4 place-content-center">
-  <div class="col-start-1 col-span-6 text-center bg-[#525F4F] w-screen h-76 ">
-    <img class="w-screen h-76" src="https://www.homedeal.nl/wp-content/uploads/sites/3/2023/02/Hovenier-Almere.jpg" alt="image description">
-  </div>
-  <div class="col-start-1 col-end-3 text-center ">02</div>
-  <div class="col-end-7 col-span-2 text-center">03</div>
-  <div class="col-start-1 col-end-7 text-center ">04</div>
-</div>  
+	<div class="col-start-1 col-span-6 text-center bg-[#525F4F] w-screen h-76">
+		<img
+			class="w-screen h-76"
+			src="https://www.homedeal.nl/wp-content/uploads/sites/3/2023/02/Hovenier-Almere.jpg"
+			alt="image description"
+		/>
+	</div>
+	<div class="col-start-1 col-end-3 text-center">02</div>
+	<div class="col-end-7 col-span-2 text-center">03</div>
+	<div class="col-start-1 col-end-7 text-center">04</div>
+</div>
