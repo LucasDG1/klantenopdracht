18e497853b63b66b9613bc8b4b7bd13e3b12fc3d
Markp33 <marrrk700@yahoo.com>
2024-06-07 10:29:26 +0200

    fbvr

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,5 +1,5 @@
 
-  <div class="fixed bottom-0 left-0 w-full bg-gray-800 text-white text-center p-4">
+  <div class="left-0 w-full bg-gray-800 text-white text-center p-4">
     © 2024 Your Company. All rights reserved.
   </div>
   
