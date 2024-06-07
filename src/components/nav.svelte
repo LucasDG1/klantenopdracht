88ceb1d993f02c3c4a6b9b9411383fa7b2fe942e
TheRealGeno <genokooijman@gmail.com>
2024-06-07 09:49:29 +0200

    nav bar better

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,16 +1,24 @@
-<nav class="w-screen fixed top-0 z-50">
-	<div class=" w-full bg-black">
-		<p class="bg-black text-white">
-			Track work across the enterprise through an open, collaborative platform. Link issues across
-			Jira and ingest data from other software development tools, so your IT support and operations
-			teams have richer contextual information to rapidly respond to requests, incidents, and
-			changes.
-		</p>
-		<hr class="h-1 my-8 bg-gray-200 border-0 " />
-		<p class="bg-black text-white">
-			Deliver great service experiences fast - without the complexity of traditional ITSM
-			solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease,
-			with a complete audit trail for every change.
-		</p>
+<nav class=" grid grid-cols-2w-screen fixed top-0 z-50">
+	<div class="bg-white w-32">
+		<img
+			class="w-full h-32"
+			src="https://cdn.discordapp.com/attachments/1247088455469764670/1248542844965355530/logo-removebg-preview.jpg?ex=66640ba3&is=6662ba23&hm=66f8c300901dc2fb9305cf321190bc65125263e915d48540a4429381bdb4b2c1&"
+			alt="image description"
+		/>
+	</div>
+	<div class="col-start-2 w-screen bg-black">
+		<div>2</div>
+		<hr class="h-1 my-8 m-16 bg-[#525F4F] border-0" />
+		<div class="flex gap-32 bg-black">
+			<div><a href="" class="text-white">Home</a></div>
+			<div><a href="" class="text-white">Reviews</a></div>
+			<div><a href="" class="text-white">Inspiratie</a></div>
+			<div><a href="" class="text-white">Nieuws Brief</a></div>
+			<div><a href="" class="text-white">Tuinaanleg</a></div>
+			<div class="flex">
+				<i class="fa fa-mobile text-white" aria-hidden="true"></i>
+				<a href="" class="text-white"> 06 5823512</a>
+			</div>
+		</div>
 	</div>
 </nav>
