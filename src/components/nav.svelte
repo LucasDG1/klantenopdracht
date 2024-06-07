7216bd5e64997d0ad437cba4ae635208a380a634
TheRealGeno <genokooijman@gmail.com>
2024-06-07 08:49:57 +0200

    footer file

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,12 +1,12 @@
 <nav class="w-screen fixed top-0 z-50">
-	<div class=" w-full">
+	<div class=" w-full bg-black">
 		<p class="bg-black text-white">
 			Track work across the enterprise through an open, collaborative platform. Link issues across
 			Jira and ingest data from other software development tools, so your IT support and operations
 			teams have richer contextual information to rapidly respond to requests, incidents, and
 			changes.
 		</p>
-		<hr class="bg-[#525F4F] border-1 dark:bg-black" />
+		<hr class="h-1 my-8 bg-gray-200 border-0 " />
 		<p class="bg-black text-white">
 			Deliver great service experiences fast - without the complexity of traditional ITSM
 			solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease,
