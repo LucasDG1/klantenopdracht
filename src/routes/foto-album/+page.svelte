e012fbb72392f8a1a502fad196bf5eddca9ac3d1
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-06-12 12:21:16 +0200

    foto-album af

--- src/routes/foto-album/+page.svelte
+++ src/routes/foto-album/+page.svelte
@@ -1,1 +1,22 @@
-<script>import Nav from '../components/nav.svelte';</script>
+<script>
+ import img1 from "../../image/a8db60_8430792b04494ef1985704c9dda7049d_mv2.webp";
+ import img2 from "../../image/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp";
+ import img3 from "../../image/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp";
+ import img4 from "../../image/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2.jpeg";
+ import img5 from "../../image/a8db60_b84128c7e416498ebd0c8972d3604240_mv2.jpeg";
+ import img6 from "../../image/a8db60_fbe67e59c7c94339903b8a24155922f0_mv2.jpeg";
+ import img7 from "../../image/Site foto mv2 (1).jpeg";
+ import img8 from "../../image/Site foto mv2.jpeg";
+ import img9 from "../../image/Site foto's mv2.webp";
+</script>
+<div class="flex flex-col items-center">
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img1}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img2}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img3}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img4}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img5}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img6}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img7}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7"><img alt="img" src={img8}></div>
+    <div class="border-solid border-green-800 border-4 rounded mt-7 mb-7"><img alt="img" src={img9}></div>
+</div>
