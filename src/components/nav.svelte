510ab362fd6a798c0c8e03f29a4694735ae1c076
TheRealGeno <genokooijman@gmail.com>
2024-06-13 08:45:17 +0200

    add feature/nav

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -11,11 +11,11 @@
 <div class={`navbar fixed top-0 w-full z-50 overflow-hidden bg-black transition-[padding] duration-400 ${y >= 80 ? 'p-[0px_0px]' : 'p-[10px_0px]'}`}>
 	<div class="grid grid-cols-2">
   <img   
-  class="{`logo w-32 h-32 col-start-1 ${y > 80 ? 'w-16 h-16' : 'w-32 h-32'}`}"
+  class="{`logo w-32 h-32 col-start-1 ${y > 80 ? 'w-16 h-16 mt-[20px]' : 'w-32 h-32'}`}"
   src={logo}
   alt="image2"
    />
-  <div id="navbar-right" class="float-right col-start-2 mr-[20px] mt-[50px]">	
+  <div id="navbar-right" class="float-right col-start-2 mr-[20px] mt-[50px] pb-[20px]">	
     <a class="active text-white text-center p-[12px] no-underline text-[18px] leading-[25px] rounded-[4px] hover:bg-[#A9A9A9] hover:text-white" href="#home">Home</a>
     <a class="text-white text-center p-[12px] no-underline text-[18px] leading-[25px] rounded-[4px] hover:bg-[#A9A9A9] hover:text-white" href="#contact">Inspiratie</a>
     <a class="text-white text-center p-[12px] no-underline text-[18px] leading-[25px] rounded-[4px] hover:bg-[#A9A9A9] hover:white" href="#about">Nieuws Brief</a>
