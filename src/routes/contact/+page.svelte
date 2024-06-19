748aec5fc55b5ed0513ca927803d867824e15acb
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-06-19 10:57:48 +0200

    contact page af

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex justify-center items-center ">
+<div class="w-full max-w-[600px] bg-white rounded-lg drop-shadow-2xl p-6 mt-32 mb-32">
+    <h2 class="text-2xl font-bold text-gray-800 mb-4">Contact</h2>
+
+    <form class="flex flex-col">
+      <input type="text" class="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Naam">
+      <input type="email" class="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Email">
+      <input type="number" class="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Telefoon Nummer">
+      <textarea name="message" class=" bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Vraag/Opmerking"></textarea>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467.21537309347747!2d4.4915169216857525!3d52.154014344699924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c68c86a8ade7%3A0x2232cb6d16180028!2sHoefstraat%2042%2C%207143hh%2C%202311%20PT%20Leiden!5e0!3m2!1snl!2snl!4v1718617713386!5m2!1snl!2snl"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="text-3xl mb-2 mt-4">Telefoon: +31 6 12345678</div>
+      <div class="text-2xl mb-2 mt-2">Beschikbaarheid:</div>
+      <div class="text-2xl mb-2">Maandag - vrijdag: 07:00 - 17:00</div>
+      <div class="text-2xl mb-2">Zaterdag: Op afspraak</div>
+      <div class="text-2xl mb-2">Zondag: Gesloten</div>
+      
+      <button type="submit" class="bg-gradient-to-r from-green-500 to-green-600 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-green-500 transition ease-in-out duration-150">Submit</button>
+    </form>
+  </div>
+</div>
