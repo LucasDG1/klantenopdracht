dc846605fc5b221dc61f8f58096ae31d872788dc
TheRealGeno <genokooijman@gmail.com>
2024-06-21 08:42:19 +0200

    delete part unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -155,15 +155,4 @@
 			</p>
 		</div>
 	</div>
-
-	<!-- <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-6 bg-[#525F4F] border-0" />
-
-		<div
-			class="flex flex-col items-start col-span-1 md:col-start-2 md:col-span-4 lg:col-start-2 lg:col-span-2 xl:col-start-2 xl:col-span-2 w-full md:w-64"
-		>
-			<h2 class="text-3xl font-bold">Hallo</h2>
-			<div class="text-2xl font-bold">Hallo2</div>
-			<hr class="my-6 w-full md:w-64 rounded bg-[#525F4F] border-1" />
-			<p class="text-sm">blablabalbalbalabl</p>
-		</div> -->
 </div>
