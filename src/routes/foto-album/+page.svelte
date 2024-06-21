d7b52d4c5f79741cdcbcdb1aa6d295329445de2d
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-06-21 10:42:28 +0200

    fotoalbum helemaal af en klaar

--- src/routes/foto-album/+page.svelte
+++ src/routes/foto-album/+page.svelte
@@ -1,35 +1,87 @@
 <script>
- import img1 from "../../image/a8db60_8430792b04494ef1985704c9dda7049d_mv2.webp";
- import img2 from "../../image/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp";
- import img3 from "../../image/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp";
- import img4 from "../../image/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2.jpeg";
- import img5 from "../../image/a8db60_b84128c7e416498ebd0c8972d3604240_mv2.jpeg";
- import img6 from "../../image/a8db60_fbe67e59c7c94339903b8a24155922f0_mv2.jpeg";
- import img7 from "../../image/Site foto mv2 (1).jpeg";
- import img9 from "../../image/Site foto's mv2.webp";
-</script> 
+	import img1 from '../../image/a8db60_8430792b04494ef1985704c9dda7049d_mv2.webp';
+	import img2 from '../../image/a8db60_0f63450d75f645bdab574047c56cf898_mv2.webp';
+	import img3 from '../../image/a8db60_89729f86eabe4bf3b65d2f554cfecd6d_mv2.webp';
+	import img4 from '../../image/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2.jpeg';
+	import img5 from '../../image/a8db60_b84128c7e416498ebd0c8972d3604240_mv2.jpeg';
+	import img6 from '../../image/a8db60_fbe67e59c7c94339903b8a24155922f0_mv2.jpeg';
+	import img7 from '../../image/Site foto mv2 (1).jpeg';
+	import img9 from "../../image/Site foto's mv2.webp";
+</script>
 
+<div class="container mx-auto p-4 mt-16">
 
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-48  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+			“Heel erg bedankt voor de efficiënte service, je was heel snel klaar en ik zal je nummer zeker
+			behouden om je weer te gebruiken.” - Hella Hoes
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-24"><img src={img1} alt="foto" /></div>
 
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
 
-<div class="bg-green-100">
-    <div class="flex items-center justify-center gap-10 max-lg:flex-col">
-        <div class="mt-36 "><img alt="img" src={img1}></div>
-        <div class="mt-24"><img alt="img" src={img2}></div>
-    </div>
-    <div class="flex items-center justify-center gap-48 max-xl:gap-10 max-lg:flex-col">
-        <div class="mt-10 "><img alt="img" src={img5}></div>
-        <div class="mt-10 "><img alt="img" src={img6}></div>
-    </div>
-    <div class="flex items-center justify-center gap-40 max-xl:gap-10 max-lg:flex-col">
-        <div class="mt-10 ga"><img alt="img" src={img3}></div>
-        <div class="mt-10 h-7/12 w-7/12 max-sm:w-10/12 max-sm:h10/12"><img alt="img" src={img4}></div>
-    </div>
-    <div class="flex items-center justify-center gap-48 max-xl:gap-10 max-lg:flex-col">
-        <div class="mt-10"><img alt="img" src={img7}></div>
-        <div class="mt-10 mb-36"><img alt="img" src={img9}></div>
-    </div>
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-48  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+            “Ga alsjeblieft door met de bezoeken aan het huis van mijn vader, want je doet geweldig werk!” -
+            Henk Haak
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-24"><img src={img6} alt="foto" /></div>
+
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-2  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+            “Zoals u weet ben ik altijd tevreden geweest met de service die u de afgelopen jaren heeft
+            verleend. Vertel me alstublieft wanneer u klaar bent om volgend jaar weer te beginnen met het
+            maaien van mijn gazons, aangezien ik graag uw diensten wil blijven ontvangen.” - Hans Hogendijk
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-24"><img src={img2} alt="foto" /></div>
+
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-10  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+            “Bedankt voor de grondige opruimbeurt die u aan mijn tuin heeft uitgevoerd. De tuin is er enorm
+            van opgeknapt en weer bruikbaar gemaakt. Nogmaals bedankt.” - Hugo van Heren
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-2"><img src={img7} alt="foto" /></div>
+
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-48  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+            “Mijn tuin was een mijn jungle voor en achter. Ik ben zo blij met het resultaat; u heeft zo hard
+            gewerkt en alles mooi en netjes achtergelaten. Aarzel niet om mijn opmerkingen te gebruiken in
+            toekomstige advertenties. Nogmaals bedankt voor al je harde werk en efficiëntie!” - Helga Hagel
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-24"><img src={img5} alt="foto" /></div>
+
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-8 gap-4 place-items-start mt-8 w-full">
+	<div class="flex flex-col items-start mt-48  md:col-start-1 lg:col-start-2 md:col-span-1 w-full md:w-64">
+		<p class="text-xl">
+            “We zijn erg blij met ons nieuwe dak van de schuur en het andere werk dat tot een zeer hoge
+            standaard is voltooid. Heel erg bedankt voor uw medewerking!” - Hopke Havermout-Hoeksteen
+        </p>
+	</div>
+	<div class="w-full md:w-[400px] flex flex-col drop-shadow-2xl md:col-start-4 lg:col-start-5 md:col-span-1 mt-24"><img src={img9} alt="foto" /></div>
+
+    <hr class="h-1 my-6 w-full md:w-full rounded col-span-1 md:col-span-8 bg-[#525F4F] border-0" />
+</div>
 </div> 
-
-
-
